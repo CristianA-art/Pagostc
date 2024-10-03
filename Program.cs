2165ee8f669ce15dd91e27b79bdb1e1bd8fedabe
USER <USER@LAPTOP-MAI9O57L>
2024-10-02 17:02:33 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PagosTc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string codigo = DateTime.Now.ToString("yymmss");
+            Console.WriteLine(codigo);
+
+            Console.WriteLine("Introduce tu codigo de verificación");
+            string codigoEscrito = Console.ReadLine();
+
+            if (codigoEscrito == codigo)
+
+                Console.WriteLine("ok");
+
+            else
+
+                Console.WriteLine("codigo incorrecto");
+                
+     
+        }
+    }
+}
